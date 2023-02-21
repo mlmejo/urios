2aecd035bb32d0d823eca3c7b23c54fff1c094d9
mlmejo <mlmejo12@gmail.com>
2023-02-21 21:25:18 +0800

    added padding to collapse items

--- resources/js/Layouts/DashboardLayout.jsx
+++ resources/js/Layouts/DashboardLayout.jsx
@@ -21,13 +21,25 @@ export default function DashboardLayout({ auth, errors, children }) {
           <div className="flex flex-col gap-y-2 py-6">
             <Item title="Dashboard" Icon={HomeIcon} />
             <Collapse title="Data" Icon={CircleStackIcon}>
-              <Item title="Accounts" Icon={UserGroupIcon} />
-              <Item title="Sections" Icon={BuildingOfficeIcon} />
+              <Item title="Accounts" Icon={UserGroupIcon} className="pl-6" />
+              <Item
+                title="Sections"
+                Icon={BuildingOfficeIcon}
+                className="pl-6"
+              />
             </Collapse>
             <Collapse title="Create" Icon={PencilIcon}>
-              <Item title="Accounts" Icon={UserGroupIcon} />
-              <Item title="Sections" Icon={BuildingOfficeIcon} />
-              <Item title="Import CSV" Icon={DocumentTextIcon} />
+              <Item title="Accounts" Icon={UserGroupIcon} className="pl-6" />
+              <Item
+                title="Sections"
+                Icon={BuildingOfficeIcon}
+                className="pl-6"
+              />
+              <Item
+                title="Import CSV"
+                Icon={DocumentTextIcon}
+                className="pl-6"
+              />
             </Collapse>
             <Item title="Activity Log" Icon={ClockIcon} />
           </div>
@@ -38,9 +50,11 @@ export default function DashboardLayout({ auth, errors, children }) {
   );
 }
 
-function Item({ title, Icon }) {
+function Item({ title, Icon, className }) {
   return (
-    <a className="font-base group flex cursor-pointer items-center gap-x-4 rounded-md p-2 hover:bg-indigo-900">
+    <a
+      className={`font-base group flex cursor-pointer items-center gap-x-4 rounded-md p-2 hover:bg-indigo-900 ${className}`}
+    >
       <Icon className="h-6 w-6 text-gray-500 group-hover:text-white" />
       <span className="group-hover:text-white">{title}</span>
     </a>
@@ -66,7 +80,7 @@ function Collapse({ title, Icon, children }) {
         )}
       </div>
       {!collapse && children ? (
-        <div className="flex flex-col gap-y-2 pl-8">{children}</div>
+        <div className="flex flex-col gap-y-2">{children}</div>
       ) : (
         <></>
       )}
